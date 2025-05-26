701e11eb306e7cd6b68b7dc93dbf3b6dd079b626
Maria Koczwara <mkoczwara@poczta.onet.pl>
2025-05-26 17:39:15 +0200

    Implement data modification with redux

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -6,16 +6,12 @@ import { useSelector } from 'react-redux';
 const Article = props => {
     const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id));
 
-    const addCard = (newColumn, columnId) => {
-        props.action(newColumn, columnId);
-    }
-
     return (<article className={styles.column}>
         <h3 className={styles.title}><span className={styles.icon + " fa fa-" + props.icon}></span> {props.title}</h3>
         <ul className={styles.cards}>
             {cards.map(card => <Card key={card.id} title={card.title} />)}
         </ul>
-        <CardForm columnId={props.id} action={addCard} />
+        <CardForm columnId={props.id} />
     </article>);
 };
 
