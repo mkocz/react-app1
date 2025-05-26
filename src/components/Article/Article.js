66c6d1b465ca4da73729738147de794a92b410b1
Maria Koczwara <mkoczwara@poczta.onet.pl>
2025-05-26 14:02:50 +0200

    Implement redux

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,8 +1,10 @@
 import styles from './Article.module.scss';
 import Card from '../Card/Card';
 import CardForm from '../CardForm/CardForm';
+import { useSelector } from 'react-redux';
 
 const Article = props => {
+    const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id));
 
     const addCard = (newColumn, columnId) => {
         props.action(newColumn, columnId);
@@ -11,7 +13,7 @@ const Article = props => {
     return (<article className={styles.column}>
         <h3 className={styles.title}><span className={styles.icon + " fa fa-" + props.icon}></span> {props.title}</h3>
         <ul className={styles.cards}>
-            {props.cards.map(card => <Card key={card.id} title={card.title} />)}
+            {cards.map(card => <Card key={card.id} title={card.title} />)}
         </ul>
         <CardForm columnId={props.id} action={addCard} />
     </article>);
